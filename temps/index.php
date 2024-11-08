94452bc64c8939678584e240ae9fc975aa794004
Hygor Hernane <hygorhernane@gmail.com>
2024-11-08 11:51:29 -0300

    Update index.php

--- temps/index.php
+++ temps/index.php
@@ -8,7 +8,7 @@
 
 <table>
 <tr>
-    <td>Enter the temperature to convert:</td>
+    <td>Insira a temperature a ser convertida:</td>
     <td><input type="text" name="temp" id="temp" size="10"></td>
 </tr>
 
